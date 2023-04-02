5ffca62166c6da185db7be553e0fb5c95f5421b5
niksirbi <niko.sirbiladze@gmail.com>
2023-04-02 16:03:37 +0100

    label release with v0.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, UCL'
 author = 'Neuroinformatics Unit'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
